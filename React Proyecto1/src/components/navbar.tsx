c00c2f3ba50f569528d164d83caae40d4bc4e014
judit Bardon <juditbardonromero@gmail.com>
2024-08-27 17:29:56 +0100

    created pages and installed browserRouter

--- React Proyecto1/src/components/navbar.tsx
+++ React Proyecto1/src/components/navbar.tsx
@@ -9,13 +9,18 @@ function Navbar (props: MenuProps) {
     const commonClasses = "gap-6 text-white w-full justify-end items-center mr-8"
     const sidebarClasses = "flex flex-col"
     const headerClasses = "hidden md:flex "
-    const classess = `${commonClasses} ${isSidebarOpen ? sidebarClasses : headerClasses}`
+    const classes = `${commonClasses} ${isSidebarOpen ? sidebarClasses : headerClasses}`
+
+    const user = null;
 
     return (
-        <nav className = {classess}>
+        <nav className = {classes}>
             <a className=" hover:text-black" href="">Home</a>
-            <a className=" hover:text-black" href="">Libros</a>
-            <a className=" hover:text-black" href="">Log in</a>
+            {user && <a className=" hover:text-black" href="">My books</a>}
+            {!user && <a className=" hover:text-black" href="">Register</a>}
+            {!user && <a className=" hover:text-black" href="">Log in</a>}
+            {user && <a className=" hover:text-black" href="">Profile</a>}
+            {user && <a className=" hover:text-black" href="">Add book</a>}
         </nav> 
     )
 }
---
+++ React Proyecto1/src/pages/AddBookPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddBookPage() {
+    return (
+        <div>
+            <h1>Esto es la página para añadir libros</h1>
+        </div>
+    )
+}
+
+export default AddBookPage
